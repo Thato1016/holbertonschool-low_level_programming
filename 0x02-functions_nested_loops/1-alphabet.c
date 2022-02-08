7ada4cba28f1597dbafcb29dd2fa1ec6c197ee38
Thato1016 <98318441+Thato1016@users.noreply.github.com>
2022-02-08 14:43:45 +0200

    Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
-/*
- * File: 1-alphabet.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *print_alphabet - Make the alphabet
+ *
+ *Return: void
+ *
  */
 void print_alphabet(void)
 {
-	char letter;
+char i;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
