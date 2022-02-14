975d635e2ef26f340b9ede0e5dbf610bb225dcbb
Thato1016 <tb.khosa@gmail.com>
2022-02-14 10:35:00 +0200

    “Pointers”

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - random  numbers
+ *
+ * Return: 0 Always
+*/
+
+int main(void)
+{
+	int i;
+	int j = 0;
+	int randnum;
+	char p[100];
+
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		randnum = random() % 127;
+		if (randnum > 32)
+		{
+			p[j] = randnum;
+			j++;
+		}
+	}
+	p[j] = '\0';
+	printf("%s", p);
+	return (0);
+}
