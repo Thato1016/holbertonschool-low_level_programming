08bebc454d138f3cbc9e84848aeab0dc322bdd1e
Thato1016 <tb.khosa@gmail.com>
2022-02-14 10:20:38 +0200

    “Pointers”

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* leet - a function that encodes a string into 1337
+* @s: a pointer string given by main
+*
+* Description: changes 'let' to '137' o to 0
+* Return: the char array
+*/
+
+char *leet(char *s)
+{
+	int i = 0;
+	int j;
+	int array[10][2] = {{97, 52},
+	{65, 52},
+	{101, 51},
+	{69, 51},
+	{108, 49},
+	{76, 49},
+	{111, 48},
+	{79, 48},
+	{116, 55},
+	{84, 55}};/* AELOT */
+
+	while (s[i] != 0)
+	{
+		for (j = 0 ; j < 10; j++)
+		{
+			if (s[i] == array[j][0])
+				s[i] = array[j][1];
+		}
+		i++;
+	}
+	return (s);
+}
