479747295d0b3579562b7c66e25010b578dd013d
Thato1016 <tb.khosa@gmail.com>
2022-02-23 11:33:56 +0200

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,63 @@
 #include "main.h"
 
-int check_palindrome(char *s);
+/**
+* _strlen - returns the length of a string
+* @s: a string given by main
+*
+* Description: This will use recursion and no loops
+*	to achieve a goal or mimic a function
+* Return: it returns the string length
+*/
+
+int _strlen(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
 
 /**
-  * is_palindrome - Returns if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int is_palindrome(char *s)
+* check_it - this will do all the work
+* @s: the char string
+* @i: the beignning of the string
+* @j: the end of the string
+*
+* Description: this loops back if the two index values
+* Return: this returns as a recursive function
+*	or it returns 1 or 0 depending on the if statements
+*/
+int check_it(char *s, int i, int j)
 {
-	if (*s == '0')
+	if (i >= j || s[i] == 0)
 		return (1);
-
-	return (check_palindrome(s));
+	if (*s != 0 && s[i] != s[j])
+		return (0);
+	return (check_it(s, i + 1, j - 1));
 }
 
-/**
-  * check_palindrome - Check if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
 
 /**
-  * _strlen_recursion - Get the length of a string
-  * @s: the string to get the length
-  *
-  * Return: the string length
-  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
+* is_palindrome - calls on my helper function check_it
+* @s: the char string
+*
+* Description: this will return 1 if the string is 0 or 1 in length
+*	if it is longer we call on the helper function and use their return
+* Return: either 1 or 0 dpeenind on if it is a palindrome
+*/
 
-	s++;
-	return (_strlen_recursion(s) + 1);
+int is_palindrome(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("abcdecba");
+	printf("%d\n", r);
+	return (0);
 }
