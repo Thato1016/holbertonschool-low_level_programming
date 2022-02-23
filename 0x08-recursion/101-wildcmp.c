60493723744a665a6fce9d9f10ae5637cd390ece
Thato1016 <tb.khosa@gmail.com>
2022-02-23 13:35:53 +0200

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,29 @@
 #include "main.h"
 
 /**
-* wildcmp - write a function that compares two strings
-* @s1: substring one pointer
-* @s2 :substring two pointer
-*
-* Description: the code returns 1 or 0
-* Return: returns 1 or 0 depending
-*/
-
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int wildcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
-		return (0);
-	return (1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	return (0);
 }
